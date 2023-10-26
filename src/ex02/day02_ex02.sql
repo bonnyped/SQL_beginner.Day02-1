ebef324a45a2e658bf0b04675cc3df1de3f4779a
bonnyped <bonnyped@student.21-school.ru>
2023-10-26 15:41:09 +0600

    minor changes

---
+++ src/ex02/day02_ex02.sql
@@ -0,0 +1,23 @@
+WITH period AS (
+    SELECT pv.person_id,
+           pv.pizzeria_id,
+           pv.visit_date
+    FROM person_visits pv
+    WHERE pv.visit_date BETWEEN '2022-01-01' AND '2022-01-03'
+),
+     all_pizzerias AS (SELECT prd.person_id,
+                              prd.visit_date,
+                              zz.name
+                       FROM pizzeria zz
+                                FULL OUTER JOIN period prd ON prd.pizzeria_id = zz.id
+     )
+
+SELECT COALESCE(res.person_name, '-'),
+       res.visit_date,
+       COALESCE(res.pizzeria_name, '-')
+FROM (SELECT p.name  person_name,
+             ap.visit_date,
+             ap.name pizzeria_name
+      FROM person p
+               FULL OUTER JOIN all_pizzerias ap ON p.id = ap.person_id) AS res
+ORDER BY 1, 2, 3;
