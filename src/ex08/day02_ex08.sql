c5dffce7095a5050bb25d6979717e8e53d5d7749
bonnyped <bonnyped@student.21-school.ru>
2023-10-26 20:40:44 +0600

    minor changes

---
+++ src/ex08/day02_ex08.sql
@@ -0,0 +1,12 @@
+WITH name_and_pizza AS (SELECT name,
+                          pizza_name
+                   FROM person p
+                            JOIN person_order po on p.id = po.person_id
+                            JOIN menu mn ON mn.id = po.menu_id
+                   WHERE gender = 'male'
+                       AND address = 'Moscow'
+                      OR address = 'Samara')
+SELECT name
+FROM name_and_pizza nam
+WHERE pizza_name = 'mushroom pizza' OR pizza_name = 'pepperoni pizza'
+ORDER BY name DESC;
